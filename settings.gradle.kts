e4d228ac2f5fb691afc9a602647bb34085927564
Taras Zubrei <taraszubrei@gmail.com>
2023-07-21 21:54:30 +0300

    feat(repo): Configure local gradle build cache for CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,13 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version("0.4.0")
 }
 
+buildCache {
+    local {
+        directory = File(rootDir, "build-cache")
+        removeUnusedEntriesAfterDays = 30
+    }
+}
+
 rootProject.name = "pet-clinic-monorepo"
 include("services:customers-service")
 include("services:vets-service")
